f760bc9d3c1c3e3409b7a52ac493b6315b040447
biostochastics <sergey@biostochastics.com>
2026-04-05 20:24:05 -0700

    feat: public API re-exports in package __init__

--- src/codon_topo/__init__.py
+++ src/codon_topo/__init__.py
@@ -1,1 +1,7 @@
 """Codon Geometry Validation & Prediction Engine."""
+from codon_topo.core.encoding import codon_to_vector, hamming_distance, ALL_CODONS
+from codon_topo.core.genetic_codes import STANDARD, get_code, all_table_ids
+from codon_topo.core.filtration import analyze_filtration
+from codon_topo.core.homology import disconnection_catalogue, persistent_homology
+from codon_topo.core.embedding import embed_codon
+from codon_topo.core.fano import is_fano_line, fano_partner
